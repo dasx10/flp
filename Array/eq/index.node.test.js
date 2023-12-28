acf8b9cb8a48961303e145e9d10459eb99b156f6
dasx10 <dasx10@gmail.com>
2023-12-29 00:23:28 +0200

    add case eq array

--- Array/eq/index.node.test.js
+++ Array/eq/index.node.test.js
@@ -10,11 +10,21 @@ describe("array eq", () => {
   });
 
   it("current value", () => {
-    assert.deepStrictEqual(eq([4, 5, 6])([1, 2, 3]), false);
-    assert.deepStrictEqual(eq([1, 2, 3])([1, 2, 3]), true);
-    assert.deepStrictEqual(eq([1, 2, 3])([1, 2, 3, 4]), false);
-    assert.deepStrictEqual(eq([1, 2, 3, 4])([1, 2, 3]), false);
-    assert.deepStrictEqual(eq([1, 2, 3, 4])([1, 2, 3, 4]), true);
-    assert.deepStrictEqual(eq([1, 2, 3, 5, 4])([1, 2, 3, 4, 5]), false);
+    assert.strictEqual(eq([4, 5, 6])([1, 2, 3]), false);
+    assert.strictEqual(eq([1, 2, 3])([1, 2, 3]), true);
+    assert.strictEqual(eq([1, 2, 3])([1, 2, 3, 4]), false);
+    assert.strictEqual(eq([1, 2, 3, 4])([1, 2, 3]), false);
+    assert.strictEqual(eq([1, 2, 3, 4])([1, 2, 3, 4]), true);
+    assert.strictEqual(eq([1, 2, 3, 5, 4])([1, 2, 3, 4, 5]), false);
+  });
+
+  it("current value object", () => {
+    assert.strictEqual(eq([{}])([{}]), true);
+    assert.strictEqual(eq([{a:1}])([{a:1}]), true);
+    assert.strictEqual(eq([{a:1}])([{a:2}]), false);
+    assert.strictEqual(eq([{a:1}])([{a:1}, {a:2}]), false);
+    assert.strictEqual(eq([{a:1}, {a:2}])([{a:1}, {a:2}]), true);
+    assert.strictEqual(eq([{a:1}, {a:2}])([{a:1}, {a:3}]), false);
+    assert.strictEqual(eq([{a:3}, {a:2}, {a:1}])([{a:1}, {a:2}, {a:3}]), false);
   });
 });
