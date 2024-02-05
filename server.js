4bfde977f331c5b8fb1513a4036a2fefaba25b9d
dasx10 <dasx10@gmail.com>
2024-02-05 17:44:22 +0200

    add test server

--- server.js
+++ server.js
@@ -1,32 +1,25 @@
-import http from "http";
-import fs   from "fs";
+import http    from "http";
+import fs      from "fs";
 import memoize from "./Decorators/memoize/index.js";
+import match   from "./Logic/match/index.js";
 
-var file = memoize((url) => fs.promises.readFile(url, "utf-8"));
-file("./index.html");
-
-var match = (value) => (key) => value[key];
-
-var contentType = match({
-  "css"  : "text/css",
-  "html" : "text/html",
-  "js"   : "application/javascript",
-  "json" : "application/json",
-});
-
-http.createServer((request, response) => {
-  var url = request.url || "/";
-  if (url == "/") url = "/index.html";
-  url = "." + url;
-
-  file(url).then((html) => {
-    console.log(url, file);
-    response.writeHead(200, { "Content-Type": contentType(url.split(".").at(-1)) });
-    response.end(html);
-  }).catch((error) => {
-    console.log(error, url);
-    response.writeHead(404);
-    response.end();
+(() => {
+  var file = memoize((url) => fs.promises.readFile(url, "utf-8"));
+  var contentType = match({
+    "css"  : "text/css",
+    "html" : "text/html",
+    "js"   : "application/javascript",
+    "json" : "application/json",
   });
 
-}).listen(8080);
+  http.createServer((request, response) => {
+    var url = (request.url === "/") ? "./index.html" : "." + request.url;
+    file(url).then((value) => {
+      response.writeHead(200, { "Content-Type": contentType(url.split(".").at(-1)) });
+      response.end(value);
+    }).catch((error) => {
+      response.writeHead(404);
+      response.end();
+    });
+  }).listen(8080);
+})();
