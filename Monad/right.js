f8964efd0459262d498e36a66b9813926c9f4d68
dasx10 <dasx10@gmail.com>
2024-06-26 19:00:28 +0300

    add check function

--- Monad/right.js
+++ Monad/right.js
@@ -1,1 +1,1 @@
-var o=(x)=>(x.then=x),i=(x)=>o((y,e)=>i(x.then(y,e))),right=(x)=>(x&&x.then)?(x.then===x)?x:i(x):o((y)=>right(y(x)));export default right;
+var o=(x)=>(x.then=x),i=(x)=>o((y,e)=>i(y&&y.construnctor===Function?x.then(y,e):x.then((x)=>x(y),e))),right=(x)=>(x&&x.then)?(x.then===x)?x:i(x):o((y)=>right(y&&y.constructor===Function?y(x):x(y)));export default right;
--- i.js
+++ i.js
@@ -3,6 +3,11 @@ import maybe, {nothing} from "./Monad/maybe.js";
 import _ from "./index.js";
 
 var log = x => (console.log(x), x);
-maybe(1).then(log).then(log);
+var _log = _(log);
+var one = _(1);
+
+_log(one);
+_log(maybe(1))
+
 // nothing.then(x => console.log(x));
 
